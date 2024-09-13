ad003d6b19a5bf2a761e36ebc558cac2d686d1f6
YesYesMe <65725419+YesYesMe0321@users.noreply.github.com>
2024-09-13 17:11:03 +0900

    Update serializers

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,6 +3,8 @@ from .models import Article, Comment
 
 # 댓글 관련 시리얼라이저
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True)
+    
     class Meta:
         model = Comment
         fields = ['id', 'user', 'article', 'text', 'created_at']
@@ -10,9 +12,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 # 기사 관련 시리얼라이저 (댓글 포함)
 class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username', read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
-        fields = ['id', 'title', 'description', 'isFake', 'comments']
+        fields = ['id', 'title', 'description', 'isFake', 'user', 'comments'] 
         read_only_fields = ['user', 'created_at']
